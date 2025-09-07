ddf207b6ec178488246c8beaf8e19ae346fe4891
Zishu <MominZishan05@gmail.com>
2025-09-07 14:54:48 +0530

    Changes done in  Home and Nav bar

--- src/Components/HomePage.jsx
+++ src/Components/HomePage.jsx
@@ -4,40 +4,41 @@ import { useDispatch } from "react-redux";
 import { addtocart } from "../Features/cartSlice";
 
 const HomePage = () => {
-    const [data,setData] = useState([]);
-    
-    const dispatch = useDispatch();
-  const fetchdata = async () => {
+  const [data, setData] = useState([]);
+  const dispatch = useDispatch();
 
+  const fetchdata = async () => {
     const userdata = await fetch("https://dummyjson.com/products");
-    const responce = await userdata.json()
+    const responce = await userdata.json();
     setData(responce.products);
     console.log(responce.products);
   };
-  
+
   useEffect(() => {
     fetchdata();
   }, []);
 
   return (
     <>
-      <h1>I am Home Page </h1>
-      <h2>Here is the User-Products.....</h2>
-      <div>
-        {
-            data.map((value,index)=>(
-                <div className="card-box" key={index}>
-                    <div className="card-img">
-                        <img src={value.thumbnail} alt="" />
-                    </div>
-                    <div className="card-title">{value.title}</div>
-                    <div className="card-price">{value.price}</div>
-                    <button onClick={()=>dispatch(addtocart(value))}>Add-To-Cart</button>
-                </div>
-            ))
-        }
+      <h1>I am Home Page</h1>
+      <h2>Here are the User Products...</h2>
+      
+      <div className="cards-container">
+        {data.map((value, index) => (
+          <div className="card-box" key={index}>
+            <div className="card-img">
+              <img src={value.thumbnail} alt={value.title} />
+            </div>
+            <div className="card-title">{value.title}</div>
+            <div className="card-price">${value.price}</div>
+            <button onClick={() => dispatch(addtocart(value))}>
+              Add To Cart
+            </button>
+          </div>
+        ))}
       </div>
     </>
   );
 };
+
 export default HomePage;
