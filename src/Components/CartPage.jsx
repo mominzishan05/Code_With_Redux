184c811f0c3cc4f408c9216ee0a16ae4d28eb653
Zishu <MominZishan05@gmail.com>
2025-09-07 15:00:52 +0530

    Changes done in CartPage

--- src/Components/CartPage.jsx
+++ src/Components/CartPage.jsx
@@ -1,36 +1,38 @@
 import React from "react";
 import "../Css/CartPage.css";
-import { useSelector,useDispatch } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
 import { removecart } from "../Features/cartSlice";
 
 const CartPage = () => {
-    const dispatch = useDispatch();
-    const showdata = useSelector((state) => state.carts.items);
-
+  const dispatch = useDispatch();
+  const showdata = useSelector((state) => state.carts.items);
 
   return (
     <>
-   
-   {showdata.length === 0 ? (
-  <p>No Items Selecteed .....</p>
-) : (
-  <div>
-    {showdata.map((item, index) => (
-      <div key={index}>
-        <div className="img-box">
-            <img src={item.thumbnail} alt="" />
-        </div>
-        <h3>{item.title}</h3>
-        <p>₹ {item.price}</p>
-        <button onClick={() => dispatch(removecart(item.id))}>
-          Remove
-        </button>
-      </div>
-    ))}
-  </div>
-)}
+      <h1>My Cart</h1>
 
+      {showdata.length === 0 ? (
+        <div className="empty-cart">
+          <p>No Items Selected...</p>
+        </div>
+      ) : (
+        <div className="cart-container">
+          {showdata.map((item, index) => (
+            <div className="cart-item" key={index}>
+              <div className="img-box">
+                <img src={item.thumbnail} alt={item.title} />
+              </div>
+              <h3>{item.title}</h3>
+              <p>₹ {item.price}</p>
+              <button onClick={() => dispatch(removecart(item.id))}>
+                Remove
+              </button>
+            </div>
+          ))}
+        </div>
+      )}
     </>
   );
 };
+
 export default CartPage;
