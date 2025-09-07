e98b219fb1e25b21ee0f83ffbf4e7c9bea2078a3
Zishu <MominZishan05@gmail.com>
2025-09-07 15:50:23 +0530

    nav updated

--- src/Routers/Navbar.jsx
+++ src/Routers/Navbar.jsx
@@ -1,19 +1,23 @@
 import React from "react";
 import "../Css/Navbar.css";
 import { Link } from "react-router-dom";
+import { useSelector } from "react-redux";
 
 const Navbar = () => {
-    
+  const cartItems = useSelector((state) => state.carts.items); // Redux cart items
+  const cartCount = cartItems.length; 
+
   return (
-    <>
-   <div className="nav-box">
-    <ul>
+    <div className="nav-box">
+      <ul>
         <Link to="/"><li>Home</li></Link>
-        <Link to="/cart"><li>Cart</li></Link>
+        <Link to="/cart">
+          <li>Cart {cartCount > 0 && `(${cartCount})`}</li>
+        </Link>
         <Link to="/login"><li>Login</li></Link>
-    </ul>
-   </div>
-    </>
+      </ul>
+    </div>
   );
 };
+
 export default Navbar;
