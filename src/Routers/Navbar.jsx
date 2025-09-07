58e297ce49af91dfa314ddc62ff313af6f2a762e
Zishu <MominZishan05@gmail.com>
2025-09-07 16:00:13 +0530

    make nav Responsive

--- src/Routers/Navbar.jsx
+++ src/Routers/Navbar.jsx
@@ -1,22 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
 import "../Css/Navbar.css";
 import { Link } from "react-router-dom";
 import { useSelector } from "react-redux";
 
 const Navbar = () => {
-  const cartItems = useSelector((state) => state.carts.items); // Redux cart items
-  const cartCount = cartItems.length; 
+  const cartItems = useSelector((state) => state.carts.items);
+  const cartCount = cartItems.length;
+  const [menuOpen, setMenuOpen] = useState(false);
 
   return (
-    <div className="nav-box">
-      <ul>
-        <Link to="/"><li>Home</li></Link>
-        <Link to="/cart">
-          <li>Cart {cartCount > 0 && `(${cartCount})`}</li>
+    <nav className="navbar">
+      <div className="logo">MyCompany</div>
+      <div className={`nav-links ${menuOpen ? "open" : ""}`}>
+        <Link to="/" onClick={() => setMenuOpen(false)}>Home</Link>
+        <Link to="/cart" onClick={() => setMenuOpen(false)}>
+          Cart {cartCount > 0 && <span className="cart-count">{cartCount}</span>}
         </Link>
-        <Link to="/login"><li>Login</li></Link>
-      </ul>
-    </div>
+        <Link to="/login" onClick={() => setMenuOpen(false)}>Login</Link>
+      </div>
+      <div
+        className={`hamburger ${menuOpen ? "active" : ""}`}
+        onClick={() => setMenuOpen(!menuOpen)}
+      >
+        <span></span>
+        <span></span>
+        <span></span>
+      </div>
+    </nav>
   );
 };
 
